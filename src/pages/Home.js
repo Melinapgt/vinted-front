3291d528f8dc8ef76992baea3a466d3a8547a77b
Melinapgt <poignetmelina@gmail.com>
2022-02-09 15:25:51 +0100

    First commit 15:25

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import "../App.css";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <LastOffer />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/Offer.js
@@ -0,0 +1,7 @@
+import "../App.css";
+
+const Offer = () => {
+  return <div>Offer Component</div>;
+};
+
+export default Offer;
