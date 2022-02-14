776a1fddef217d09e52e2451d3e5ccee91736a90
Melinapgt <poignetmelina@gmail.com>
2022-02-15 00:23:04 +0100

    pages, corrections

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,6 +8,9 @@ const Home = (props) => {
   const { search, sort, setSort, toggle, range, token } = props;
   const [data, setData] = useState();
   const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(1);
+
+  const limit = 10;
 
   useEffect(() => {
     const fetchData = async () => {
@@ -26,7 +29,7 @@ const Home = (props) => {
         setIsLoading(false);
       } else {
         const response = await axios.get(
-          `https://lereacteur-vinted-api.herokuapp.com/offers?sort=${sort}&priceMin=${range[0]}&priceMax=${range[1]}`
+          `https://lereacteur-vinted-api.herokuapp.com/offers?limit=${limit}&page=${page}&sort=${sort}&priceMin=${range[0]}&priceMax=${range[1]}`
         );
         console.log("response.data==>", response.data);
         setData(response.data);
@@ -34,14 +37,22 @@ const Home = (props) => {
       }
     };
     fetchData();
-  }, [search, sort, setSort, toggle, range]);
+  }, [search, sort, setSort, toggle, range, page]);
 
   return isLoading ? (
     <div>En cours de chargement...</div>
   ) : (
     <div>
       <Hero token={token} />
+      <span className="articles-count">Articles</span>
+      <span className="count">{data.count} résultats</span>
       <LastOffer data={data} />
+      <div className="pages">
+        {page > 1 && (
+          <button onClick={() => setPage(page - 1)}>Page précédent</button>
+        )}
+        <button onClick={() => setPage(page + 1)}>Page suivante</button>
+      </div>
     </div>
   );
 };
