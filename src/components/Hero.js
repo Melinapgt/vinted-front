e9605d3b5ff84fc7df9b2b2f1d2ef2f03366781c
Melinapgt <poignetmelina@gmail.com>
2022-06-01 12:10:55 +0200

    corrections bug deployment netlifly banner hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,24 +1,26 @@
 import "../App.css";
 import { Link } from "react-router-dom";
+import banner from "../asset/img/banner.jpg";
 
 const Hero = (props) => {
   const { token } = props;
   return (
     <div className="hero">
-      <div className="container">
-        <div className="hero-bloc">
-          Prêts à faire du tri dans vos plaquards?
-          {token ? (
-            <Link to="/publish">
-              <button>Commencer à vendre</button>
-            </Link>
-          ) : (
-            <Link to="/login">
-              <button>Commencer à vendre</button>
-            </Link>
-          )}
-        </div>
+      <img src={banner} alt="" />
+      {/* <div className="container"> */}
+      <div className="hero-bloc">
+        Prêts à faire du tri dans vos plaquards?
+        {token ? (
+          <Link to="/publish">
+            <button>Commencer à vendre</button>
+          </Link>
+        ) : (
+          <Link to="/login">
+            <button>Commencer à vendre</button>
+          </Link>
+        )}
       </div>
+      {/* </div> */}
     </div>
   );
 };
